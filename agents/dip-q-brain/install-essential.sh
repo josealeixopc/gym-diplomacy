5c37d1c8e4d9a7176e4b3a6a499ab55964b591c5
José Aleixo <josealeixo.pc@gmail.com>
2019-05-17 16:11:15 +0100

    updated install essential

--- agents/dip-q-brain/install-essential.sh
+++ agents/dip-q-brain/install-essential.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This helps Docker image build faster, because TF and Gym will never change and can be kept on cache
-pipenv install tensorflow~=1.13 gym~=0.12 --skip-lock
+pipenv install tensorflow~=1.13 gym~=0.12 stable-baselines~=2.5.1 --skip-lock
