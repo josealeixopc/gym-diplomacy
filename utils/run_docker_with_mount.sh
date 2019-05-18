6cc6c617546afb5ea5bbcee930fc257b21f9e04d
José Aleixo <josealeixo.pc@gmail.com>
2019-05-18 04:27:55 +0100

    Added cmd o docker file. Change nego length

--- utils/run_docker_with_mount.sh
+++ utils/run_docker_with_mount.sh
@@ -3,7 +3,7 @@
 # This will run the dip-q image, while mounting the current folder on the host's /tmp/dip, meaning that any change to that folder
 # will be reflected on the host's folder as well.
 
-docker run -d \
+docker run \
   -it \
   --rm \
   --name dip-q \
