3b0b9cd9fb11626a40a6b6aa1fecec4e2b330186
José Aleixo <josealeixo.pc@gmail.com>
2019-05-19 22:24:25 +0100

    Reduce number of games because it might overflow.

--- utils/run_docker_with_mount.sh
+++ utils/run_docker_with_mount.sh
@@ -3,9 +3,8 @@
 # This will run the dip-q image, while mounting the current folder on the host's /tmp/dip, meaning that any change to that folder
 # will be reflected on the host's folder as well.
 
-docker run \
-  -it \
+docker run -it \
   --rm \
   --name dip-q \
-  --mount type=bind,source="$(pwd)",target="/tmp/dip" \
+  --mount type=bind,source="$(pwd)/dip-out",target="/tmp/dip-out" \
   registry.gitlab.com/jazzchipc/dip-q:latest
